e6be1c2374b982d8d8a03b429a86ad538ad2af9b
oliver <olnor18@student.sdu.dk>
2022-12-06 10:09:37 +0100

    Fix misspelling

--- Backend/src/Routes/DebugRoutes.ts
+++ Backend/src/Routes/DebugRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
-export const debugRouter = express.Router();
+export const debugRoutes = express.Router();
 
 async function sleep(time: number) {
     return new Promise((res, rej) => setTimeout(res, time))
 }
 
-debugRouter.get("/stress", async (req, res) => {
+debugRoutes.get("/stress", async (req, res) => {
     for (let i = 0; i < 1000; i++){
         let a = 2^100*3
         await sleep(10)
